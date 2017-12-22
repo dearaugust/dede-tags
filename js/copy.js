89fac3017574c557511b2b74a6dc0ff383a33d2e
dearaugust <419491539@qq.com>
2017-12-22 13:52:41 +0800

    first commit

---
+++ js/copy.js
@@ -0,0 +1,28 @@
+var cfg = {
+	path: "ZeroClipboard.swf",
+	copy: function(){
+	return $(this).siblings().text();
+	},
+	beforeCopy:function(){
+		$(this).css("color","orange");
+	},
+	afterCopy:function(){
+		var $copysuc = $('<div class="alert alert-success" role="alert">复制成功</div>');
+		$("body").find(".alert").remove().end().append($copysuc);
+		$copysuc.css({
+			"position":"fixed",
+			"z-index":"999",
+			"bottom":"50%",
+			"left":"50%",
+			"margin":"0 0 -20px -80px"
+		});
+		$(".alert").fadeOut(2000);
+		$(this).css("color","");
+	}
+};
+$(document).ready(function(){
+    $(".taglink a").mouseover(function(){
+		console.log($(this).siblings().text())
+		if(!$(this).data('init')){$(this).zclip(cfg);$(this).data('init',true)}
+	})
+});
